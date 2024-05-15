43b5ce68fa65f1ad59bf20eeaee746eeba4ae450
rosafigueroaA01748086 <105660271+rosafigueroaA01748086@users.noreply.github.com>
2024-05-15 00:54:31 -0600

    Cambios Estatus

--- src/AppAgente/componentes/EstatusLlamada.js
+++ src/AppAgente/componentes/EstatusLlamada.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import '../styles/estatusLlamada.css';
 import Semaforo from './Semaforo';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
@@ -18,6 +18,9 @@ import enojado from '../images/enojado.PNG';
 const EstatusLlamada = () => {
     const [sentimiento, setSentimiento] = useState(normal);
 
+    const [tiempo, setTiempo] = useState(0);
+    const [corriendo, setCorriendo] = useState(false);
+
     const cambiarSentimiento = () => {
         if (sentimiento === feliz) {
             setSentimiento(normal);
@@ -28,6 +31,59 @@ const EstatusLlamada = () => {
         }
     };
 
+    const toggleContador = () => {
+        setCorriendo(!corriendo);
+        // setTiempo(0); // Reiniciar el tiempo cuando se inicia el contador
+    };
+    
+    useEffect(() => {
+        let intervalId;
+    
+        if (corriendo) {
+            intervalId = setInterval(() => {
+                setTiempo(tiempo => tiempo + 10); 
+            }, 10); 
+        } else {
+            clearInterval(intervalId);
+        }
+    
+        return () => clearInterval(intervalId);
+    }, [corriendo]);
+    
+    const tiempoFormateado = () => {
+        const minutos = Math.floor(tiempo / 60000);
+        const segundos = Math.floor((tiempo % 60000) / 1000);
+    
+        const formatMinutos = minutos < 10 ? `0${minutos}` : minutos;
+        const formatSegundos = segundos < 10 ? `0${segundos}` : segundos;
+    
+        return `${formatMinutos}:${formatSegundos}`;
+    };
+
+    const obtenerClaseTiempo = () => {
+        if (tiempo < 46000) {
+            return 'bueno';
+        } else if (tiempo >= 46000 && tiempo < 60000) {
+            return 'normal';
+        } else {
+            return 'malo';
+        }
+    };
+
+    //Solicitar el numero de telefono
+    // const descargar = useCallback(
+    //     () => {
+    //       console.log("Descargando datos");
+    //       fetch('/api')
+    //       .then((response) => response.json())
+    //       .then((data) => {
+    //       //Manejo de datos
+    //       })
+    //       .catch((error) => console.log(error));
+    //     },);
+
+    //Solicitar el sentimiento de connect
+
     return (
         <div className='llamada'>
             <div className='columnaE'>
@@ -36,8 +92,8 @@ const EstatusLlamada = () => {
                         <LocalPhoneIcon className='icon' />
                         <h3 className='tel' id='tel'>+52 5577499543</h3>
                     </div>
-                    <div className='normal' id='tiempo'>
-                        <AccessTimeFilledIcon /> <h3> 1:57:03</h3>
+                    <div className={`${obtenerClaseTiempo()}`} id='tiempo' onClick={toggleContador}>
+                        <AccessTimeFilledIcon /> <h3>{tiempoFormateado(tiempo)}</h3>
                     </div>
                 </div>
                 <div className='control'>
@@ -56,11 +112,10 @@ const EstatusLlamada = () => {
                         <div className='sentimiento'>
                             <img src={sentimiento} alt="sentimiento" className='sentimiento' onClick={cambiarSentimiento} />
                         </div>
-                        <Semaforo />
-                    </div>
+                        <Semaforo tiempo={tiempo} />                    </div>
                 </div>
-                <Estadistica/>
                 <SolicitarAyuda/>
+                <Estadistica/>
             </div>
         </div>
         
--- src/AppAgente/componentes/SolicitarAyuda.js
+++ src/AppAgente/componentes/SolicitarAyuda.js
@@ -3,10 +3,14 @@ import React from 'react'
 import { Button } from '@mui/material';
 
 const SolicitarAyuda = () => {
+  
+  const solicitarAyuda = () => {
+      console.log('Ayuda solicitada')
+  }
     return (
       <div className='solicitar'>
         <h3>¿Necesitas ayuda de un supervisor?</h3>
-        <Button className='botonAyuda'>Solicitar</Button>
+        <Button className='botonAyuda' onClick={solicitarAyuda}>Solicitar</Button>
       </div>
       )
 }
