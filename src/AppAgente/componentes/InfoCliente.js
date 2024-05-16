7919748434ab91681ade42d81b56f4b41a62b133
A01753505 <A01753505@tec.mx>
2024-05-16 13:12:55 -0600

    Intento nombre y telefono del cliente

--- src/AppAgente/componentes/InfoCliente.js
+++ src/AppAgente/componentes/InfoCliente.js
@@ -2,14 +2,39 @@ import "../styles/infoCliente.css";
 import Llamadas from "./Llamadas";
 import Transacciones from "./Transacciones";
 import Datos from "./Datos";
+import { useCallback, useState } from "react";
 
 const Contenedor = () => {
+  const [cliente, setCliente] = useState([]);
+  const url = "http://localhost:8080/cliente/consultar?id=1";
+
+  const descargar = useCallback(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Datos obtenidos del servidor:", data);
+        const dataCliente = {
+          nombre: data.nombre,
+          telefono: data.telefono,
+        };
+        setCliente(dataCliente);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    console.log("Descargando datos");
+    descargar();
+  }, [descargar]);
+
   return (
     <div className="container">
       <Datos
         datosCliente={{
-          nombre: "Guillermo Morelos Ramírez",
-          telefono: "+52 1 55 1234 5678",
+          // nombre: "Guillermo Morelos Ramírez",
+          // telefono: "+52 1 55 1234 5678",
+          nombre: cliente.nombre,
+          telefono: cliente.telefono,
           cuenta: "1234567890",
           tarjeta: "Platino Express",
           saldo: "$20,493.00",
@@ -29,9 +54,24 @@ const Contenedor = () => {
       <Llamadas
         numLlamadas={8}
         listaLlamadas={[
-          { title: "Aclaraciones", description: "Cobro de $5,000.00 desconocido", stars: 5, date: "20/03/2024" },
-          { title: "Aclaraciones", description: "Pago de tarjeta de crédito", stars: 2, date: "15/01/2024" },
-          { title: "Aclaraciones", description: "Pago de tarjeta de crédito", stars: 4, date: "08/12/2023" }
+          {
+            title: "Aclaraciones",
+            description: "Cobro de $5,000.00 desconocido",
+            stars: 5,
+            date: "20/03/2024",
+          },
+          {
+            title: "Aclaraciones",
+            description: "Pago de tarjeta de crédito",
+            stars: 2,
+            date: "15/01/2024",
+          },
+          {
+            title: "Aclaraciones",
+            description: "Pago de tarjeta de crédito",
+            stars: 4,
+            date: "08/12/2023",
+          },
         ]}
       />
     </div>
