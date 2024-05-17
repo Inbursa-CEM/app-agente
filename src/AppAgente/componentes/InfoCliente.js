372be8d2d27570b1d999ac9dd156810785e152bf
A01753505 <A01753505@tec.mx>
2024-05-16 19:27:16 -0600

    Back al 70 de infoCliente

--- src/AppAgente/componentes/InfoCliente.js
+++ src/AppAgente/componentes/InfoCliente.js
@@ -6,7 +6,11 @@ import { useContext } from "react";
 import { ContextoInfo } from "./ProveedorInfoCliente";
 
 const Contenedor = () => {
-  const [cliente, tarjeta, arrTransacciones] = useContext(ContextoInfo);
+  const [cliente, tarjeta, arrTransacciones, arrLlamadas, numLlamadas] =
+    useContext(ContextoInfo);
+
+  // MODIFICAR
+  const numCalls = arrLlamadas.length;
 
   return (
     <div className="container">
@@ -24,29 +28,7 @@ const Contenedor = () => {
         // FALTANTE
         elementoDestacadoIndex={2}
       />
-      <Llamadas
-        numLlamadas={8}
-        listaLlamadas={[
-          {
-            title: "Aclaraciones",
-            description: "Cobro de $5,000.00 desconocido",
-            stars: 5,
-            date: "20/03/2024",
-          },
-          {
-            title: "Aclaraciones",
-            description: "Pago de tarjeta de crédito",
-            stars: 2,
-            date: "15/01/2024",
-          },
-          {
-            title: "Aclaraciones",
-            description: "Pago de tarjeta de crédito",
-            stars: 4,
-            date: "08/12/2023",
-          },
-        ]}
-      />
+      <Llamadas numLlamadas={numCalls} listaLlamadas={arrLlamadas} />
     </div>
   );
 };
--- src/AppAgente/componentes/ProveedorInfoCliente.js
+++ src/AppAgente/componentes/ProveedorInfoCliente.js
@@ -6,10 +6,14 @@ const ProveedorInfoCliente = ({ children }) => {
   const [cliente, setCliente] = useState([]);
   const [tarjeta, setTarjeta] = useState([]);
   const [arrTransacciones, setTransacciones] = useState([]);
+  const [arrLlamadas, setLlamadas] = useState([]);
+  const [numLlamadas, setNumLlamadas] = useState([]);
   const urlCliente = "http://localhost:8080/cliente/consultar?id=1";
   const urlTarjeta = "http://localhost:8080/tarjeta/consultar?id=1";
   const urlTransacciones =
     "http://localhost:8080/transaccion/consultar?numCuenta=123456";
+  const urlLlamadas = "http://localhost:8080/llamada/consultar";
+  const urlNumLlamadas = "http://localhost:8080/llamada/numLlamadas";
 
   useEffect(() => {
     console.log("Descargando datos");
@@ -46,17 +50,49 @@ const ProveedorInfoCliente = ({ children }) => {
           const transaccion = [
             transax.nombre,
             transax.detalle,
-            "$" + transax.monto + ".00"
+            "$" + transax.monto + ".00",
           ];
           return transaccion;
         });
         setTransacciones(arrNuevo);
       })
       .catch((error) => console.log(error));
+
+    fetch(urlLlamadas)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Datos obtenidos de las llamadas:", data);
+        const arrNuevo = data.map((llamada) => {
+          const call = {
+            tema: llamada.tema,
+            motivo: llamada.motivo,
+            fecha:
+              llamada.fechaInicio.substring(8, 10) +
+              "/" +
+              llamada.fechaInicio.substring(5, 7) +
+              "/" +
+              llamada.fechaInicio.substring(0, 4),
+            stars: llamada.nivelSatisfaccion,
+          };
+          return call;
+        });
+        setLlamadas(arrNuevo);
+      })
+      .catch((error) => console.log(error));
+
+    fetch(urlNumLlamadas)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Número de llamadas en el mes:", data);
+        setNumLlamadas(data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <ContextoInfo.Provider value={[cliente, tarjeta, arrTransacciones]}>
+    <ContextoInfo.Provider
+      value={[cliente, tarjeta, arrTransacciones, arrLlamadas, numLlamadas]}
+    >
       {children}
     </ContextoInfo.Provider>
   );
