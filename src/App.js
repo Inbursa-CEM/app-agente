5d8656a6ea742c91a39a1923ab334f9b49b1cfbd
A01753505 <A01753505@tec.mx>
2024-05-21 15:57:45 -0600

    Connect embedido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import EstatusLlamada from "./AppAgente/componentes/EstatusLlamada";
 import "./Styles/App.css";
 import InterfazGuia from "./AppAgente/Guia/components/InterfazGuia";
 import InfoCliente from "./AppAgente/componentes/InfoCliente";
-// import Connect from "./AppAgente/componentes/Connect";
+import Connect from "./AppAgente/componentes/Connect";
 import ProveedorInfoCliente from "./AppAgente/componentes/ProveedorInfoCliente";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
             <InfoCliente />
           </ProveedorInfoCliente>
         </div>
-        <div className="contenedor4">{/* <Connect /> */}</div>
+        <div className="contenedor4"><Connect /></div>
       </div>
     </div>
   );
